ff6566b0a41f76e633fe7341fd2a923a25a8ccd3
Leander Rysanek <leander@Leanders-Air.lan>
2019-06-14 08:49:33 +0200

    refactor(server): redirect client to servers on localhost

---
+++ OBJECTIVES_LEGACY.md
@@ -0,0 +1,11 @@
+Backend:
+
+- Call specific user (see SimpleWebRTC, PeerJS)
+- Time synchronicity to be handled in server, not in client
+- Rewrite servers in Socket.io & Express instead of WebSockets & bare Node
+
+Frontend:
+
+- Handle video screen size
+- Reduce Countdown + CountDownClient & CallPane + CallPaneClient components
+- Implement a router for rendering (see REACH router)
--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,8 @@
-const SOCKET_SERVER_BASE_URL = 'https://fleeting-sock-server-mvp110619.herokuapp.com/'
-const CALL_SERVER_BASE_URL = 'wss://fleeting-call-server-mvp110619.herokuapp.com/'
+const SOCKET_SERVER_BASE_URL = 'https://localhost:4001';
+const CALL_SERVER_BASE_URL = 'https://localhost:8444';
+
+// const SOCKET_SERVER_BASE_URL = 'https://fleeting-sock-server-mvp110619.herokuapp.com/'
+// const CALL_SERVER_BASE_URL = 'wss://fleeting-call-server-mvp110619.herokuapp.com/'
 
 export {
   SOCKET_SERVER_BASE_URL,
