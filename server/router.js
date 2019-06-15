fa4cd57effe88528d33b1e4630e3cc143f67de04
Leander Rysanek <leander@Leanders-Air.lan>
2019-06-15 11:43:42 +0200

    jwt

--- server/router.js
+++ server/router.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else res.sendStatus(403);
+};
+
 router.post('/signup', controller.postNewUser);
+router.post('/login', controller.postLoginUser);
+router.post('/test', verifyToken, controller.postSthElse);
 
 module.exports = router;
