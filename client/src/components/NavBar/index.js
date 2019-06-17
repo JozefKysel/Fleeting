d032edb10402b943495f9eff9700c27d85ef0f5e
Leander Rysanek <leander@Leanders-Air.lan>
2019-06-17 16:48:36 +0200

    fix(client navbar): fix search bug

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -10,12 +10,12 @@ function NavBar({userData}) {
   const [contacts, setContact] = useState({});
 
   const handleSearch = (e) => {
-    api.searchForUsers(e.target.value)
-      .then(res => res.json())
-      .then(res => {
-        console.log(res);
-        setContact(res)})
-      .catch(e => console.log(e));
+    if (e.target.value) {
+      api.searchForUsers(e.target.value)
+        .then(res => res.json())
+        .then(res => setContact(res))
+        .catch(e => console.log(e));
+    }
   }
 
   return (
@@ -27,7 +27,7 @@ function NavBar({userData}) {
       <Search placeholder="search contacts" onChange={handleSearch} enterButton />
       <br>
       </br>
-      {Object.keys(contacts).length > 0 && <Contact className="searchContact" contact={contacts}/>}
+      {contacts && Object.keys(contacts).length > 0 && <Contact className="searchContact" contact={contacts}/>}
       </div>
     </div>
   );
